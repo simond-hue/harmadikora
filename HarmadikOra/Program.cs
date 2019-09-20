51b7600fa62e8f23ccee5352c94abdb4ad98ccd8
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 12:11:01 +0200

    masodik feladat

--- HarmadikOra/Program.cs
+++ HarmadikOra/Program.cs
@@ -8,29 +8,41 @@ namespace HarmadikOra
 {
     class Program
     {
-        static void Main(string[] args) 
+        static int[] Input(int darabszam)
         {
             string input = "";
-            Console.Write("Adjon meg 10 számot (space-ekkel elválasztva): ");
+            Console.Write("Adjon meg az előbb megadott darbmennyiségű számot (space-ekkel elválasztva): ");
             input = Console.ReadLine();
             string[] inputSplit = input.Split(' ');
-            int[] tomb = new int[10];
-            for(int i = 0; i < tomb.Length; i++)
+            int[] tomb = new int[darabszam];
+            for (int i = 0; i < tomb.Length; i++)
             {
                 tomb[i] = Convert.ToInt32(inputSplit[i]);
             }
+            return tomb;
+        }
 
+        static int Max(int[] tomb)
+        {
             int max = tomb[0];
-
-            for(int i = 1; i < tomb.Length; i++)
+            for (int i = 1; i < tomb.Length; i++)
             {
-                if(tomb[i] > max)
+                if (tomb[i] > max)
                 {
                     max = tomb[i];
                 }
             }
+            return max;
+        }
 
-            Console.WriteLine("A legnagyobb elem: " + max);
+        static void Main(string[] args) 
+        {
+            Console.Write("Adja meg az elemszámot: ");
+            int darabszam = Convert.ToInt32(Console.ReadLine());
+
+            var tomb = Input(darabszam);
+
+            Console.WriteLine("A legnagyobb elem: " + Max(tomb));
             Console.ReadKey();
         }
     }
