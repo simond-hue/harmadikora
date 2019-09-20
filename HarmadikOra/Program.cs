76fa49c617bbd26a7f36be9cd9dca7aaeec65214
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 12:03:46 +0200

    start

--- HarmadikOra/Program.cs
+++ HarmadikOra/Program.cs
@@ -10,7 +10,27 @@ namespace HarmadikOra
     {
         static void Main(string[] args) 
         {
-            Console.WriteLine("XDDDDDDDDDDDDDDDDDDDDDDDD");
+            string input = "";
+            Console.Write("Adjon meg 10 számot (space-ekkel elválasztva): ");
+            input = Console.ReadLine();
+            string[] inputSplit = input.Split(' ');
+            int[] tomb = new int[10];
+            for(int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = Convert.ToInt32(inputSplit[i]);
+            }
+
+            int max = tomb[0];
+
+            for(int i = 1; i < tomb.Length; i++)
+            {
+                if(tomb[i] > max)
+                {
+                    max = tomb[i];
+                }
+            }
+
+            Console.WriteLine("A legnagyobb elem: " + max);
             Console.ReadKey();
         }
     }
